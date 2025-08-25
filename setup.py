c6bf973077df265f869110a8938b3dcd1fc0221b
MaryamNazir1 <nazirmaryam452@gmail.com>
2025-08-25 18:26:39 +0500

    setup and requirement

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPEN_E_DOT = "-e ."
+
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    this function will return the list of requirements
+    """
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+setup(
+    name="ml",
+    version="0.1.0",
+    description="A machine learning library",
+    author="Maryam",
+    author_email="nazirmaryam452@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
+    python_requires=">=3.7",
+)
